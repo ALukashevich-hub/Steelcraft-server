f16b92e62a121c45bb347d5e5f5d734e69c6a494
Andrey L <ALukashevich@tut.by>
2020-07-15 11:44:44 +0300

    feat: test fetch

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
-const express = require('express')
-const config = require('./config/config')
+const express = require('express');
+const config = require('./config/config');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 
-const app = express()
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.listen(process.env.PORT || config.port,
-    () => console.log(`Server start on port ${config.port} ...`))
+    () => console.log(`Server start on port ${config.port} ...`));
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.send({testGet: 'Got a GET request'});
+    console.log('GET COMPLITE');
+});
+app.post('/', function (req, res) {
+  res.send({testPost: 'Got a POST request'});
+  console.log('POST COMPLITE');
+  console.log(req.body);
 });
